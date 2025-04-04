6325b7ca8fee848772d8ffc3fca4114dddc22814
Adithyan T <adithyant.mail@gmail.com>
2025-04-04 17:23:00 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,12 +6,12 @@ import { FloatingDockDemo } from "./Dock";
 
 export const Experience = () => {
   return (
-    <div className="w-full md:my-20  relative flex flex-col justify-center items-center">
-      <div className=" flex px-3 max-w-7xl  items-center flex-col  mb-8 gap-6 justify-center ">
-        <h3 className="text-xs  text-white/40 font-semibold   tracking-wider font-firaSans mb-2">
+    <div className="w-full md:my-20 relative flex flex-col justify-center items-center">
+      <div className="flex px-3 max-w-7xl items-center flex-col mb-8 gap-6 justify-center">
+        <h3 className="text-xs text-white/40 font-semibold tracking-wider font-firaSans mb-2">
           CAREER
         </h3>
-        <h1 className="font-semibold font-firaSans  text-4xl md:text-6xl text-white/80">
+        <h1 className="font-semibold font-firaSans text-4xl md:text-6xl text-white/80">
           Experience
         </h1>
         <h2 className="text-md font-fira max-w-6xl px-5 text-center font-semibold tracking-wide text-white/40">
@@ -21,15 +21,15 @@ export const Experience = () => {
           the forefront of technology.
         </h2>
       </div>
-      <section className="flex w-full flex-col gap-4 p-8  justify-center items-center bg-background-dim2 rounded-xl ">
+
+      <section className="flex w-full flex-col gap-4 p-8 justify-center items-center bg-background-dim2 rounded-xl">
+        {/* Frinks AI Experience */}
         <div className="relative flex flex-col gap-4 max-w-5xl border border-primary-dark rounded-xl p-8 bg-background-dim2">
           <div className="flex flex-col gap-4 max-w-[calc(var(--grid-32)*20)]">
             <span className="flex flex-col">
-              <div className="">
-                <h3 className="-mt-1 mb-1 text-xl font-firaSans font-bold">
-                  Full Stack Intern
-                </h3>
-              </div>
+              <h3 className="-mt-1 mb-1 text-xl font-firaSans font-bold">
+                Full Stack Intern
+              </h3>
               <h4 className="text-sm mb-1 font-fira font-semibold text-white/70">
                 Frinks AI
               </h4>
@@ -44,31 +44,21 @@ export const Experience = () => {
               implementing frontend components for a seamless user experience
               and developing backend APIs for efficient data management. I
               optimized database queries, integrated authentication mechanisms,
-              and ensured the system's scalability. Additionally, I collaborated
-              with cross-functional teams to enhance system performance and
-              deployment workflows using Git and other development tools.
+              and ensured the system&apos;s scalability. Additionally, I
+              collaborated with cross-functional teams to enhance system
+              performance and deployment workflows using Git and other
+              development tools.
             </p>
           </div>
-
-          {/* <div className="">
-            <FloatingDockDemo />
-          </div> */}
-
-          {/* <Badges
-            list={fullStack}
-            block="stack"
-            fullContainer="fullContainer"
-            color={undefined}
-          /> */}
         </div>
+
+        {/* Oronium Experience */}
         <div className="relative flex flex-col gap-4 max-w-5xl border border-primary-dark rounded-xl p-8 bg-background-dim2">
           <div className="flex flex-col gap-4 max-w-[calc(var(--grid-32)*20)]">
             <span className="flex flex-col">
-              <div className="">
-                <h3 className="-mt-1 mb-1 text-xl font-firaSans font-bold">
-                  Nextjs Developer Intern
-                </h3>
-              </div>
+              <h3 className="-mt-1 mb-1 text-xl font-firaSans font-bold">
+                Nextjs Developer Intern
+              </h3>
               <h4 className="text-sm mb-1 font-fira font-semibold text-white/70">
                 Oronium
               </h4>
@@ -86,17 +76,6 @@ export const Experience = () => {
               environment.
             </p>
           </div>
-
-          {/* <div className="">
-            <FloatingDockDemo />
-          </div> */}
-
-          {/* <Badges
-            list={fullStack}
-            block="stack"
-            fullContainer="fullContainer"
-            color={undefined}
-          /> */}
         </div>
       </section>
     </div>
